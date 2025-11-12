f8bf9a77660bb75af21fc0fd81fed24700605cd3
togarci <thyago.garcia10@gmail.com>
2025-11-12 19:36:17 -0300

    started project

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+import tailwindcss from '@tailwindcss/vite';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/image', '@pinia/nuxt', '@vite-pwa/nuxt', 'pinia-plugin-persistedstate/nuxt'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+
+  app: {
+    head: {
+      title: 'Gym Workout',
+      htmlAttrs: {
+        lang: 'pt-br',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          name: 'description',
+          content: 'Um aplicativo para salvar treinos de academia',
+        },
+        { name: 'format-detection', content: 'telephone=yes' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: '',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Encode+Sans+Semi+Expanded:wght@100;200;300;400;500;600;700;800;900&display=swap',
+        },
+      ],
+    },
+  },
+});
